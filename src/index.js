6cc3cbd81fda58fc6745b5b5b1c83baa1b01ef9f
MalinTollaksen <malin.tollaksen@hotmail.com>
2019-03-11 20:43:40 +0100

    SUSU v2.1
    Lagt til tabell med informasjon om de forskjellige varene på status.

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ import {
   Nav,
   NavDropdown,
   FormControl,
-  ListGroup
+  ListGroup,
+  Table
 } from 'react-bootstrap';
 
 import createHashHistory from 'history/createHashHistory';
@@ -74,46 +75,56 @@ class BestillingNew extends Component {
       <div>
         <Card>
           <ListGroup>
-            <ListGroup.Item className="list-group-item">
-              <Form.Label> Navn: </Form.Label>
-              <Form.Control type="text" value={this.navn} onChange={e => (this.navn = e.target.value)} />
-            </ListGroup.Item>
-            <ListGroup.Item className="list-group-item">
-              <Form.Label> Email: </Form.Label>
-              <Form.Control type="text" value={this.email} onChange={e => (this.email = e.target.value)} />
-            </ListGroup.Item>
-            <ListGroup.Item className="list-group-item">
-              <Form.Label> Mobilnummer: </Form.Label>
-              <Form.Control type="text" value={this.mobilnummer} onChange={e => (this.mobilnummer = e.target.value)} />
-            </ListGroup.Item>
+            <Form.Row>
+              <ListGroup.Item className="list-group-item">
+                <Form.Label> Navn: </Form.Label>
+                <Form.Control type="text" value={this.navn} onChange={e => (this.navn = e.target.value)} />
+              </ListGroup.Item>
+              <ListGroup.Item className="list-group-item">
+                <Form.Label> Email: </Form.Label>
+                <Form.Control type="text" value={this.email} onChange={e => (this.email = e.target.value)} />
+              </ListGroup.Item>
+              <ListGroup.Item className="list-group-item">
+                <Form.Label> Mobilnummer: </Form.Label>
+                <Form.Control
+                  type="text"
+                  value={this.mobilnummer}
+                  onChange={e => (this.mobilnummer = e.target.value)}
+                />
+              </ListGroup.Item>
+            </Form.Row>
           </ListGroup>
         </Card>
         <Card>
           <ListGroup>
-            <ListGroup.Item className="list-group-item">
-              <Form.Label> Fra: </Form.Label>
-              <Form.Control type="date" value={this.fra} onChange={e => (this.fra = e.target.value)} />
-            </ListGroup.Item>
-            <ListGroup.Item className="list-group-item">
-              <Form.Label> Til: </Form.Label>
-              <Form.Control type="date" value={this.til} onChange={e => (this.il = e.target.value)} />
-            </ListGroup.Item>
+            <Form.Row>
+              <ListGroup.Item className="list-group-item">
+                <Form.Label> Fra: </Form.Label>
+                <Form.Control type="datetime-local" value={this.fra} onChange={e => (this.fra = e.target.value)} />
+              </ListGroup.Item>
+              <ListGroup.Item className="list-group-item">
+                <Form.Label> Til: </Form.Label>
+                <Form.Control type="datetime-local" value={this.til} onChange={e => (this.il = e.target.value)} />
+              </ListGroup.Item>
+            </Form.Row>
           </ListGroup>
         </Card>
         <Card>
           <ListGroup>
-            <ListGroup.Item className="list-group-item">
-              <Form.Label> Hentested: </Form.Label>
-              <Form.Control type="text" value={this.hentested} onChange={e => (this.hentested = e.target.value)} />
-            </ListGroup.Item>
-            <ListGroup.Item className="list-group-item">
-              <Form.Label> Leveringssted: </Form.Label>
-              <Form.Control
-                type="text"
-                value={this.leveringssted}
-                onChange={e => (this.leveringssted = e.target.value)}
-              />
-            </ListGroup.Item>
+            <Form.Row>
+              <ListGroup.Item className="list-group-item">
+                <Form.Label> Hentested: </Form.Label>
+                <Form.Control type="text" value={this.hentested} onChange={e => (this.hentested = e.target.value)} />
+              </ListGroup.Item>
+              <ListGroup.Item className="list-group-item">
+                <Form.Label> Leveringssted: </Form.Label>
+                <Form.Control
+                  type="text"
+                  value={this.leveringssted}
+                  onChange={e => (this.leveringssted = e.target.value)}
+                />
+              </ListGroup.Item>
+            </Form.Row>
           </ListGroup>
         </Card>
       </div>
@@ -122,19 +133,36 @@ class BestillingNew extends Component {
 }
 
 class Status extends Component {
-  kunder = [];
-
-  navn = '';
-  email = '';
-  mobilnummer = '';
+  varer = [];
 
   render() {
-    return <div />;
+    return (
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>vare ID</th>
+            <th>type</th>
+            <th>pris</th>
+            <th>status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.varer.map(varer => (
+            <tr key={varer.v_id}>
+              {varer.v_id}
+              <td key={varer.id}>{varer.type}</td>
+              <td key={varer.id}>{varer.pris}</td>
+              <td key={varer.id}>{varer.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    );
   }
 
   mounted() {
-    bestillingService.hentKunder(mobilnummer => {
-      this.kunder = mobilnummer;
+    statusService.hentVarer(varer => {
+      this.varer = varer;
     });
   }
 }
@@ -143,9 +171,9 @@ ReactDOM.render(
   <HashRouter>
     <div>
       <Menu />
-
       <Route exact path="/" component={Home} />
       <Route exact path="/bestilling" component={BestillingNew} />
+      <Route exact path="/status" component={Status} />
     </div>
   </HashRouter>,
   document.getElementById('root')
--- src/services.js
+++ src/services.js
@@ -10,7 +10,15 @@ class BestillingService {
   }
 }
 
-class StatusService {}
+class StatusService {
+  hentVarer(success) {
+    connection.query('SELECT * FROM vare INNER JOIN prisliste on vare.type = prisliste.type', (error, results) => {
+      if (error) return console.error(error);
+
+      success(results);
+    });
+  }
+}
 
 export let bestillingService = new BestillingService();
 export let statusService = new StatusService();
