6fbc5e0cbb63acc9d6a59d3cfd15fd0b32cb9dd8
MalinTollaksen <malin.tollaksen@hotmail.com>
2019-03-11 16:41:41 +0100

    SUSU v2.0
    Endret til offisiel react-bootstrap i stedet for å bruke den fra forelesningene. Mye lettere å brukere andre elementer nå som ikke har blitt brukt i forelesningen.
    
    Alt finnes her: https://react-bootstrap.github.io/components/alerts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import { Component } from 'react-simplified';
 import ReactDOM from 'react-dom';
 import { NavLink, HashRouter, Route } from 'react-router-dom';
 import { bestillingService, statusService } from './services';
-import { Card, List, Row, Column, NavBar, Button, Form } from './widgets';
+import {
+  Card,
+  List,
+  Row,
+  Column,
+  Navbar,
+  Button,
+  Form,
+  DropdownButton,
+  Dropdown,
+  Item,
+  Link,
+  Nav,
+  NavDropdown,
+  FormControl,
+  ListGroup
+} from 'react-bootstrap';
 
 import createHashHistory from 'history/createHashHistory';
 const history = createHashHistory(); // Use history.push(...) to programmatically change path, for instance after successfully saving a student
@@ -16,15 +32,16 @@ class FormLabel extends Component {
 class Menu extends Component {
   render() {
     return (
-      <NavBar brand="SUSU v1">
-        <NavBar.Link activeStyle={{ color: 'pink' }} to="/bestilling">
+      <Navbar>
+        <NavLink to="/">SUSU v2.0</NavLink>
+        <NavLink activeStyle={{ color: 'pink' }} to="/bestilling">
           Bestilling
-        </NavBar.Link>
+        </NavLink>
 
-        <NavBar.Link activeStyle={{ color: 'pink' }} to="/status">
+        <NavLink activeStyle={{ color: 'pink' }} to="/status">
           Status
-        </NavBar.Link>
-      </NavBar>
+        </NavLink>
+      </Navbar>
     );
   }
 }
@@ -32,15 +49,10 @@ class Menu extends Component {
 class Home extends Component {
   render() {
     return (
-      <Card title="Welcome">
-        Til SUSU v1.1!
+      <Card>
+        Til SUSU v2.0!
         <br />
         Dette er verdens beste informasjonssystem for utleie av sykler og utstyr.
-        <br />
-        <br />
-        Versjon 1.1 inneholder:
-        <br />
-        Bestillingsside med kundeinformasjon, dato for utleie og levering/hentested
       </Card>
     );
   }
@@ -60,45 +72,49 @@ class BestillingNew extends Component {
   render() {
     return (
       <div>
-        <Card title="Kundeinformasjon:">
-          <ul className="list-group">
-            <li className="list-group-item">
+        <Card>
+          <ListGroup>
+            <ListGroup.Item className="list-group-item">
               <Form.Label> Navn: </Form.Label>
-              <Form.Input type="text" value={this.navn} onChange={e => (this.navn = e.target.value)} />
-            </li>
-            <li className="list-group-item">
+              <Form.Control type="text" value={this.navn} onChange={e => (this.navn = e.target.value)} />
+            </ListGroup.Item>
+            <ListGroup.Item className="list-group-item">
               <Form.Label> Email: </Form.Label>
-              <Form.Input type="text" value={this.email} onChange={e => (this.email = e.target.value)} />
-            </li>
-            <li className="list-group-item">
+              <Form.Control type="text" value={this.email} onChange={e => (this.email = e.target.value)} />
+            </ListGroup.Item>
+            <ListGroup.Item className="list-group-item">
               <Form.Label> Mobilnummer: </Form.Label>
-              <Form.Input type="text" value={this.mobilnummer} onChange={e => (this.mobilnummer = e.target.value)} />
-            </li>
-          </ul>
+              <Form.Control type="text" value={this.mobilnummer} onChange={e => (this.mobilnummer = e.target.value)} />
+            </ListGroup.Item>
+          </ListGroup>
         </Card>
-        <Card title="Utleie:">
-          <ul className="list-group">
-            <li className="list-group-item">
+        <Card>
+          <ListGroup>
+            <ListGroup.Item className="list-group-item">
               <Form.Label> Fra: </Form.Label>
-              <Form.Input type="text" value={this.fra} onChange={e => (this.fra = e.target.value)} />
-            </li>
-            <li className="list-group-item">
+              <Form.Control type="date" value={this.fra} onChange={e => (this.fra = e.target.value)} />
+            </ListGroup.Item>
+            <ListGroup.Item className="list-group-item">
               <Form.Label> Til: </Form.Label>
-              <Form.Input type="text" value={this.til} onChange={e => (this.til = e.target.value)} />
-            </li>
-          </ul>
+              <Form.Control type="date" value={this.til} onChange={e => (this.il = e.target.value)} />
+            </ListGroup.Item>
+          </ListGroup>
         </Card>
-        <Card title="Sted:">
-          <ul className="list-group">
-            <li className="list-group-item">
+        <Card>
+          <ListGroup>
+            <ListGroup.Item className="list-group-item">
               <Form.Label> Hentested: </Form.Label>
-              <Form.Input type="text" value={this.lokasjon} onChange={e => (this.lokasjon = e.target.value)} />
-            </li>
-            <li className="list-group-item">
+              <Form.Control type="text" value={this.hentested} onChange={e => (this.hentested = e.target.value)} />
+            </ListGroup.Item>
+            <ListGroup.Item className="list-group-item">
               <Form.Label> Leveringssted: </Form.Label>
-              <Form.Input type="text" value={this.lokasjon} onChange={e => (this.lokasjon = e.target.value)} />
-            </li>
-          </ul>
+              <Form.Control
+                type="text"
+                value={this.leveringssted}
+                onChange={e => (this.leveringssted = e.target.value)}
+              />
+            </ListGroup.Item>
+          </ListGroup>
         </Card>
       </div>
     );
@@ -106,28 +122,20 @@ class BestillingNew extends Component {
 }
 
 class Status extends Component {
+  kunder = [];
+
   navn = '';
   email = '';
   mobilnummer = '';
 
   render() {
-    return (
-      <div />
-      // <Card title="Velg Mobilnummer">
-      //   {this.allStudents
-      //     .filter(allStudent => this.students.every(student => allStudent.id != student.id))
-      //     .map(student => (
-      //       <Row key={student.id}>
-      //         <Column>{student.name}</Column>
-      //         <Column width={2}>
-      //           <Button.Success small onClick={() => this.addStudent(student.id)}>
-      //             +
-      //           </Button.Success>
-      //         </Column>
-      //       </Row>
-      //     ))}
-      // </Card>
-    );
+    return <div />;
+  }
+
+  mounted() {
+    bestillingService.hentKunder(mobilnummer => {
+      this.kunder = mobilnummer;
+    });
   }
 }
 
@@ -135,10 +143,9 @@ ReactDOM.render(
   <HashRouter>
     <div>
       <Menu />
-      <Route exact path="/" component={Home} />
 
+      <Route exact path="/" component={Home} />
       <Route exact path="/bestilling" component={BestillingNew} />
-      <Route exact path="/status" component={Status} />
     </div>
   </HashRouter>,
   document.getElementById('root')
--- src/services.js
+++ src/services.js
@@ -1,9 +1,10 @@
 import { connection } from './mysql_connection';
 
 class BestillingService {
-  hentKunder(mobilnummer, success) {
-    connection.query('select * from Kunder where mobilnummer=?', (error, results) => {
+  hentKunder(success) {
+    connection.query('select * from kunde', (error, results) => {
       if (error) return console.error(error);
+
       success(results);
     });
   }
