620c7453b54938ac2abf125e6edf52da8239532e
MalinTollaksen <malin.tollaksen@hotmail.com>
2019-03-21 15:55:19 +0100

    SUSU v5.4
    Lagt til valgknapper på de forskjellige tingene å bestille

--- src/services.js
+++ src/services.js
@@ -171,8 +171,8 @@ class s_Sok {
   }
   LedigeSykler(fra, til, success) {
     connection.query(
-      'SELECT * FROM vare LEFT JOIN utleieliste ON utleieliste.v_id = vare.v_id LEFT JOIN bestilling ON bestilling.b_id = utleieliste.b_id LEFT JOIN sykkel ON sykkel.v_id = vare.v_id WHERE ((bestilling.fra NOT BETWEEN ? AND ?) OR (bestilling.fra IS NULL) ) AND sykkel.v_id IS NOT NULL OR ((bestilling.til NOT BETWEEN ? AND ?) OR( bestilling.til IS NULL)) AND sykkel.v_id IS NOT NULL ORDER BY `sykkel`.`v_id`  ASC',
-      [fra, til, fra, til],
+      'SELECT DISTINCT v_id, type, ramme, girsystem, størrelse_hjul, status, pris FROM tilgjengelige_sykler WHERE NOT (fra >= ? OR  til >= ?) OR (fra IS NULL OR til IS NULL)',
+      [fra, til],
       (error, results) => {
         if (error) return console.error(error);
 
@@ -182,8 +182,8 @@ class s_Sok {
   }
   LedigeSyklerType(fra, til, type, success) {
     connection.query(
-      'SELECT * FROM vare LEFT JOIN utleieliste ON utleieliste.v_id = vare.v_id LEFT JOIN bestilling ON bestilling.b_id = utleieliste.b_id LEFT JOIN sykkel ON sykkel.v_id = vare.v_id WHERE ((bestilling.fra NOT BETWEEN ? AND ?) OR (bestilling.fra IS NULL) ) AND sykkel.v_id IS NOT NULL AND sykkel.type=? OR ((bestilling.til NOT BETWEEN ? AND ?) OR( bestilling.til IS NULL)) AND sykkel.v_id IS NOT NULL AND sykkel.type=? ORDER BY `sykkel`.`v_id`  ASC',
-      [fra, til, type, fra, til, type],
+      'SELECT DISTINCT v_id, type, ramme, girsystem, størrelse_hjul, status, pris FROM tilgjengelige_sykler WHERE NOT (fra >= ? OR  til >= ?) OR (fra IS NULL OR til IS NULL) AND type = ?',
+      [fra, til, type],
       (error, results) => {
         if (error) return console.error(error);
 
@@ -193,8 +193,8 @@ class s_Sok {
   }
   LedigeUtstyr(fra, til, success) {
     connection.query(
-      'SELECT * FROM vare LEFT JOIN utleieliste ON utleieliste.v_id = vare.v_id LEFT JOIN bestilling ON bestilling.b_id = utleieliste.b_id LEFT JOIN utstyr ON utstyr.v_id = vare.v_id WHERE ((bestilling.fra NOT BETWEEN ? AND ?) OR (bestilling.fra IS NULL) ) AND utstyr.v_id IS NOT NULL OR ((bestilling.til NOT BETWEEN ? AND ?) OR( bestilling.til IS NULL)) AND utstyr.v_id IS NOT NULL ORDER BY utstyr.`v_id`  ASC',
-      [fra, til, fra, til],
+      'SELECT DISTINCT v_id, type, status, pris FROM tilgjengelige_utstyr WHERE NOT (fra >= ? OR  til >= ?) OR (fra IS NULL OR til IS NULL)',
+      [fra, til],
       (error, results) => {
         if (error) return console.error(error);
 
@@ -204,8 +204,8 @@ class s_Sok {
   }
   LedigeUtstyrType(fra, til, type, success) {
     connection.query(
-      'SELECT * FROM vare LEFT JOIN utleieliste ON utleieliste.v_id = vare.v_id LEFT JOIN bestilling ON bestilling.b_id = utleieliste.b_id LEFT JOIN utstyr ON utstyr.v_id = vare.v_id WHERE ((bestilling.fra NOT BETWEEN ? AND ?) OR (bestilling.fra IS NULL) ) AND utstyr.v_id IS NOT NULL AND utstyr.type=? OR ((bestilling.til NOT BETWEEN ? AND ?) OR( bestilling.til IS NULL)) AND utstyr.v_id IS NOT NULL AND utstyr.type=? ORDER BY utstyr.`v_id`  ASC',
-      [fra, til, type, fra, til, type],
+      'SELECT DISTINCT v_id, type, status, pris FROM tilgjengelige_utstyr WHERE NOT (fra >= ? OR  til >= ?) OR (fra IS NULL OR til IS NULL) AND type = ?',
+      [fra, til, type],
       (error, results) => {
         if (error) return console.error(error);
 
