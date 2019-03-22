f87a6d0cda3c734b438a05bbf03341767435ad95
Martypartyy <42603131+Martypartyy@users.noreply.github.com>
2019-03-22 09:52:51 +0100

    o

--- src/Innhold/hjem.js
+++ src/Innhold/hjem.js
@@ -7,7 +7,7 @@ export class Home extends Component {
     return (
       <Carousel>
         <Carousel.Item>
-          <img src="sykkel0.jpg" alt="First slide" width="100%" height="800" />
+          <img src="sykkel0.jpg" alt="First slide" width="100%" height="900" />
           <Carousel.Caption>
             <h3 />
             <p />
@@ -16,18 +16,12 @@ export class Home extends Component {
         <Carousel.Item>
           <img src="sykkel5.jpg" alt="Second slide" width="100%" height="800" />
 
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
+          <Carousel.Caption />
         </Carousel.Item>
         <Carousel.Item>
           <img src="sykkel3.jpg" alt="Third slide" width="100%" height="800" />
 
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
+          <Carousel.Caption />
         </Carousel.Item>
       </Carousel>
     );
