1d236000e06ec449a17cfa8cc6bd6e93741a652c
Martypartyy <42603131+Martypartyy@users.noreply.github.com>
2019-03-22 09:49:47 +0100

    La inn noen bilder
    la inn noen bilder

--- src/Innhold/hjem.js
+++ src/Innhold/hjem.js
@@ -1,15 +1,35 @@
 import * as React from 'react';
 import { Component } from 'react-simplified';
-import { Card } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 
 export class Home extends Component {
   render() {
     return (
-      <Card>
-        Til SUSU v5.2!
-        <br />
-        Dette er verdens beste informasjonssystem for utleie av sykler og utstyr.
-      </Card>
+      <Carousel>
+        <Carousel.Item>
+          <img src="sykkel0.jpg" alt="First slide" width="100%" height="800" />
+          <Carousel.Caption>
+            <h3 />
+            <p />
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src="sykkel5.jpg" alt="Second slide" width="100%" height="800" />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src="sykkel3.jpg" alt="Third slide" width="100%" height="800" />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     );
   }
 }
