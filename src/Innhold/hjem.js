dafe86c6a20f32ac214b6832b65ed5cd0a4f34ef
MalinTollaksen <malin.tollaksen@hotmail.com>
2019-03-27 13:44:40 +0100

    endringer 2

--- src/Innhold/hjem.js
+++ src/Innhold/hjem.js
@@ -6,7 +6,7 @@ export class Home extends Component {
   render() {
     return (
       <Card>
-        Til SUSU v7.1!
+        Til SUSU v7.3!
         <br />
         Dette er verdens beste informasjonssystem for utleie av sykler og utstyr.
       </Card>
