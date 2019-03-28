877d7c971a8d1c7aae5d2f02c3f3b7c652ca6293
MalinTollaksen <malin.tollaksen@hotmail.com>
2019-03-28 12:17:23 +0100

    SUSU v.7.7
    Mulighet for å endre status på en bestilling. Endrer også statusen på varene

--- src/Innhold/hjem.js
+++ src/Innhold/hjem.js
@@ -6,7 +6,7 @@ export class Home extends Component {
   render() {
     return (
       <Card>
-        Til SUSU v7.3!
+        Til SUSU v7.7!
         <br />
         Dette er verdens beste informasjonssystem for utleie av sykler og utstyr.
       </Card>
