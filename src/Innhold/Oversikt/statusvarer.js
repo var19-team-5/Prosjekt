304e4100743f74974e89db9e8c167b5d4d3aabee
MalinTollaksen <malin.tollaksen@hotmail.com>
2019-04-09 17:19:31 +0200

    SUSU v9.3
    Begynt å kommentere koden

--- src/Innhold/Oversikt/statusvarer.js
+++ src/Innhold/Oversikt/statusvarer.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { s_salg } from './_o_services';
 import { ListGroup, Col, Button, Form, Row } from 'react-bootstrap';
-
 import { Oversikt } from './nav';
+
 export class Statusvarer extends Oversikt {
   lager = [];
   savnet = [];
@@ -14,62 +14,52 @@ export class Statusvarer extends Oversikt {
     return [
       <React.Fragment>
         <ListGroup.Item className="list-group-item">
-        <h5>Status varer:</h5>
-        <br/>
-        <ListGroup.Item className="list-group-item">
-        <Row>
-        <Col className="text-center" xs={2}>
-          <Form.Label>På lager:</Form.Label>
-          {this.lager.map(lager => (
-            <div key={lager.lager}>{lager.lager}</div>
-          ))}
-        </Col>
-        <Col className="text-center" xs={2}>
-          <Form.Label>Savnet:</Form.Label>
-          {this.savnet.map(savnet => (
-            <div key={savnet.savnet}>{savnet.savnet}</div>
-          ))}
-        </Col>
-        <Col className="text-center" xs={2}>
-          <Form.Label>Trenger reparasjon:</Form.Label>
-          {this.trengerrep.map(trengerrep => (
-            <div key={trengerrep.trengerrep}>{trengerrep.trengerrep}</div>
-          ))}
-        </Col>
-        <Col className="text-center" xs={2}>
-          <Form.Label>På reparasjon:</Form.Label>
-          {this.pårep.map(pårep => (
-            <div key={pårep.pårep}>{pårep.pårep}</div>
-          ))}
-        </Col>
-        <Col className="text-center" xs={2}>
-          <Form.Label>Transporteres:</Form.Label>
-          {this.transp.map(transp => (
-            <div key={transp.transp}>{transp.transp}</div>
-          ))}
-        </Col>
-        </Row>
+          <h5>Status varer:</h5>
+          <br />
+          <ListGroup.Item className="list-group-item">
+            <Row>
+              <Col className="text-center" xs={2}>
+                <Form.Label>På lager:</Form.Label>
+                <div>{this.lager}</div>
+              </Col>
+              <Col className="text-center" xs={2}>
+                <Form.Label>Savnet:</Form.Label>
+                <div>{this.savnet}</div>
+              </Col>
+              <Col className="text-center" xs={2}>
+                <Form.Label>Trenger reparasjon:</Form.Label>
+                <div>{this.trengerrep}</div>
+              </Col>
+              <Col className="text-center" xs={2}>
+                <Form.Label>På reparasjon:</Form.Label>
+                <div>{this.pårep}</div>
+              </Col>
+              <Col className="text-center" xs={2}>
+                <Form.Label>Transporteres:</Form.Label>
+                <div>{this.transp}</div>
+              </Col>
+            </Row>
+          </ListGroup.Item>
         </ListGroup.Item>
-      </ListGroup.Item>
       </React.Fragment>
     ];
   }
 
   mounted() {
-    s_salg.antallPåLager(lager => {
-      this.lager = lager;
+    s_salg.AntallPåLager(lager => {
+      this.lager = lager[0].lager;
     });
-    s_salg.antallSavnet(savnet => {
-      this.savnet = savnet;
+    s_salg.AntallSavnet(savnet => {
+      this.savnet = savnet[0].savnet;
     });
-    s_salg.trengerReparasjon(trengerrep => {
-      this.trengerrep = trengerrep;
+    s_salg.AntallTrengerReparasjon(trengerrep => {
+      this.trengerrep = trengerrep[0].trengerrep;
     });
-    s_salg.antallPåReparasjon(pårep => {
-      this.pårep = pårep;
+    s_salg.AntallPåReparasjon(pårep => {
+      this.pårep = pårep[0].pårep;
     });
-    s_salg.antallTransporteres(transp => {
-      this.transp = transp;
+    s_salg.AntallTransporteres(transp => {
+      this.transp = transp[0].transp;
     });
   }
 }
