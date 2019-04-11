8367cb6184586d5959cb6a0bf6a20102bfc4e701
Martypartyy <42603131+Martypartyy@users.noreply.github.com>
2019-04-11 18:20:09 +0200

    Update info.js

--- src/Innhold/info.js
+++ src/Innhold/info.js
@@ -4,6 +4,24 @@ import {} from 'react-bootstrap';
 
 export class Info extends Component {
   render() {
-    return <div />;
+    return (
+      <React.Fragment>
+        <br />
+        <h1>Hva er SUSU?</h1>
+        <br />
+        <h6>"Sykkelutleie for sykler og utstyr"</h6>
+        <br />
+        <br />
+        <p>
+          SUSU er et informasjonssystem for administrering av sykkelutleie. SUSU er utviklet av fire studenter i
+          <br />
+          forbindelse med et prosjekt i emnet IDRI1005, Objektorientert programmering med systemarbeid. Et emne man har
+          <br />
+          i andre semester på studiet Digital Forretningsutvikling ved NTNU. Team 5 består av Ane Solberg Nielsen, Malin
+          <br />
+          Tollaksen, Sawsen Boudabous og Martin Riseth.
+        </p>
+      </React.Fragment>
+    );
   }
 }
